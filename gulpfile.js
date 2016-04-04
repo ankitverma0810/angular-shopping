970dec1129d6444ef096caaaae3fa11324e4bd01
ankit <ankitverma_0810@yahoo.co.in>
2016-04-04 22:55:10 +0530

    Updated Gulp file with test tasks

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,10 @@ var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
 	imagemin = require('gulp-imagemin'),
 	pngcrush = require('imagemin-pngcrush'),
-	concat = require('gulp-concat');
+	concat = require('gulp-concat'),
+	karma = require('karma').Server,
+	protractor = require('gulp-protractor').protractor,
+	webdriver_update = require('gulp-protractor').webdriver_update;
 
 var DEFAULT = {
 	appDir: 'components',
@@ -17,6 +20,7 @@ var DEFAULT = {
 DEFAULT.sassStyle = (DEFAULT.env === 'development') ? "expanded" : "compressed";
 DEFAULT.outputDir = (DEFAULT.env === 'development') ?  "builds/development/" : "builds/production/";
 
+//path of all the files
 var paths = {
 	scripts: [
 		'bower_components/angular/angular.js',
@@ -31,6 +35,48 @@ var paths = {
 	}
 };
 
+//Javascript task
+gulp.task('js', function() {
+	gulp.src(paths.scripts)
+		.pipe(concat('main.js'))
+		.pipe(gulpif(DEFAULT.env === 'production', uglify()))
+		.pipe(gulp.dest(DEFAULT.outputDir+'scripts'))
+		.pipe(browserSync.stream());
+});
+
+//compass task
+gulp.task('compass', function() {
+	gulp.src(paths.sass)
+		.pipe(compass({
+			sass: DEFAULT.appDir + '/sass',
+			image: DEFAULT.outputDir+'images',
+			style: DEFAULT.sassStyle
+		})
+		.on('error', gutil.log))
+		.pipe(gulp.dest(DEFAULT.outputDir+'css'))
+		.pipe(browserSync.stream());
+});
+
+//html task
+gulp.task('html', function() {
+	gulp.src('builds/development/**/*.html')
+		.pipe(gulpif( DEFAULT.env === 'production', gulp.dest(DEFAULT.outputDir) ))
+		.pipe(browserSync.stream());
+});
+
+//images task
+gulp.task('images', function() {
+	gulp.src('builds/development/images/**/*.*')
+		.pipe(gulpif( DEFAULT.env === 'production', imagemin({
+			progressive: true,
+			svgoPlugins: [{ removeViewBox: false }],
+			use: [pngcrush()]
+		})))
+		.pipe(gulpif( DEFAULT.env === 'production', gulp.dest(DEFAULT.outputDir + 'images') ))
+		.pipe(browserSync.stream());
+});
+
+//app serve and watch task
 gulp.task('serve', function() {
 	browserSync.init({
 		notify: false,
@@ -47,41 +93,51 @@ gulp.task('serve', function() {
 	gulp.watch('builds/development/images/**/*.*', ['images']);
 });
 
-gulp.task('js', function() {
-	gulp.src(paths.scripts)
-		.pipe(concat('main.js'))
-		.pipe(gulpif(DEFAULT.env === 'production', uglify()))
-		.pipe(gulp.dest(DEFAULT.outputDir+'scripts'))
-		.pipe(browserSync.stream());
+
+//test and e2e testing task starts from here
+gulp.task('serve-test', function() {
+	browserSync.init({
+		notify: false,
+		port: 8081,
+		server: {
+			baseDir: ["test", DEFAULT.outputDir],
+			routes: {
+				'/bower_components': 'bower_components'
+			}
+		}
+	});
 });
 
-gulp.task('compass', function() {
-	gulp.src(paths.sass)
-		.pipe(compass({
-			sass: DEFAULT.appDir + '/sass',
-			image: DEFAULT.outputDir+'images',
-			style: DEFAULT.sassStyle
-		})
-		.on('error', gutil.log))
-		.pipe(gulp.dest(DEFAULT.outputDir+'css'))
-		.pipe(browserSync.stream());
+gulp.task('test-browser', function(done) {
+	new karma({
+		configFile: __dirname + '/karma.conf.js',
+		singleRun: true,
+		reporters: ['mocha', 'coverage']
+	}, done).start();
 });
 
-gulp.task('html', function() {
-	gulp.src('builds/development/**/*.html')
-		.pipe(gulpif( DEFAULT.env === 'production', gulp.dest(DEFAULT.outputDir) ))
-		.pipe(browserSync.stream());
+gulp.task('serve-coverage', ['test-browser'], function() {
+	browserSync.init({
+		notify: false,
+		port: 7777,
+		server: {
+			baseDir: ["test/coverage"],
+		}
+	});
 });
 
-gulp.task('images', function() {
-	gulp.src('builds/development/images/**/*.*')
-		.pipe(gulpif( DEFAULT.env === 'production', imagemin({
-			progressive: true,
-			svgoPlugins: [{ removeViewBox: false }],
-			use: [pngcrush()]
-		})))
-		.pipe(gulpif( DEFAULT.env === 'production', gulp.dest(DEFAULT.outputDir + 'images') ))
-		.pipe(browserSync.stream());
+// Downloads the selenium webdriver
+// ensure that the driver is installed
+gulp.task('webdriver_update', webdriver_update);
+
+gulp.task('protractor', ['webdriver_update', 'serve'], function(done) {
+	gulp.src(['test/e2e/*.js'])
+		.pipe(protractor({
+			configFile: 'test/protractor.config.js'
+		}))
+		.on('error', function(e) { console.log(e) })
+		.on('end', done);
 });
 
+//default task
 gulp.task('default', ['html', 'js', 'compass', 'images', 'serve']);
