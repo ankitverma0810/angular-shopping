691981d21039c82cfadfcf1bdb19b6cdbbb747bd
ankit <ankitverma_0810@yahoo.co.in>
2016-05-24 23:14:48 +0530

    Updated files with sorting and serach functionality

--- components/scripts/controllers/page.js
+++ components/scripts/controllers/page.js
@@ -6,7 +6,6 @@ angular.module('pages')
 		$scope.index = function() {
 			$http.get(APPCONSTANTS.URL+APPCONSTANTS.API+'/pages').then(function(response) {
 	            $scope.pages = response.data;
-	            $log.log(response.data);
 	        });
 		};
 
@@ -25,4 +24,22 @@ angular.module('pages')
 				$log.log(error);
 			});
 		};
+
+		$scope.status = function(id, status, index) {
+			$http.get(APPCONSTANTS.URL+APPCONSTANTS.API+'/pages/setstatus/'+id+'/'+status).then(function(response) {
+				if(response.data.error) {
+					alertsManager.addAlert(response.data.error, 'alert-danger');
+				} else {
+					alertsManager.addAlert(response.data.success, 'alert-success');
+					$scope.pages[index]['Page']['status_id'] = status.toString();
+				}
+			}, function(error) {
+				$log.log(error);
+			});
+		};
+
+		$scope.sort = function(keyname) {
+	        $scope.sortKey = keyname;   //set the sortKey to the param passed
+	        $scope.reverse = !$scope.reverse; //if true make it false and vice versa
+	    }
 }]);
